fed0dc49d432e4ef483cc0e25a011c1eebca2e39
dana <dana.changwe@szi.gov.zm>
2020-02-07 10:21:44 +0200

    Add project files.

---
+++ Program1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloName
+{
+    public class Program1
+    {
+     public   static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+    }
+}
