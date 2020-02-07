74015be1c0da75b089f2d8eee36c0510f9a410cb
dana <dana.changwe@szi.gov.zm>
2020-02-07 10:58:22 +0200

    Feature : added strings to program

--- Program1.cs
+++ Program1.cs
@@ -7,6 +7,11 @@ namespace HelloName
      public   static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            string fname = "Dana";
+            string lname = "Changwe";
+            string nationality = "Zambian";
+            Console.WriteLine("My name is {0} {1}", fname, lname);
+            Console.WriteLine("I am {0}", nationality);
         }
 
     }
